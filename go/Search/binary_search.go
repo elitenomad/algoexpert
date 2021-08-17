a2f7a81b4628f06c24f3b10d08095ffdce461bf9
elitenomad <stalin.pranava@gmail.com>
2021-08-17 22:09:56 +1000

    Binary search in Go - Recursive

--- go/Search/binary_search.go
+++ go/Search/binary_search.go
@@ -28,8 +28,32 @@ func BinarySearchIterative(input []int, element int) int {
 	return -1
 }
 
+func BinarySearchRecursive(input []int, low, high, element int) int {
+	mid := (low + high) / 2
+
+	if input[mid] == element {
+		return mid
+	}
+
+	if low <= high {
+		if input[mid] < element {
+			return BinarySearchRecursive(input, mid+1, high, element)
+		} else if input[mid] > element {
+			return BinarySearchRecursive(input, low, mid-1, element)
+		}
+		mid = (low + high) / 2
+	}
+
+	return -1
+}
+
 func main() {
 	a := []int{10, 20, 30, 40, 50, 60}
 	fmt.Println(BinarySearchIterative(a, 50))
 	fmt.Println(BinarySearchIterative(a, 560))
+
+	low := 0
+	high := len(a) - 1
+	fmt.Println(BinarySearchRecursive(a, low, high, 50))
+	fmt.Println(BinarySearchRecursive(a, low, high, 560))
 }
