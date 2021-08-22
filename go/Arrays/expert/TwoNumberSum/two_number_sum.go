a83ef92c8e55717c8876650c652b96f08dedc85b
elitenomad <stalin.pranava@gmail.com>
2021-08-22 11:40:04 +1000

    Array and search questions

--- go/Arrays/expert/TwoNumberSum/two_number_sum.go
+++ go/Arrays/expert/TwoNumberSum/two_number_sum.go
@@ -2,7 +2,8 @@ package main
 
 import "fmt"
 
-func TwoNumberSum(array []int, target int) []int {
+// When Array is unsorted we can use hash
+func TwoNumberSum(array []int, target int) []int { // O(N) and O(N)
 	var store = make(map[int]bool)
 
 	for _, elem := range array {
@@ -18,8 +19,42 @@ func TwoNumberSum(array []int, target int) []int {
 	return []int{}
 }
 
+// Two pointer approach for sorted array
+func abs(a int) int {
+	if a < 0 {
+		return -a
+	}
+	return a
+}
+func TwoNumberSum2PointerApproach(input []int, target int) []int {
+	i := 0
+	j := len(input) - 1
+	// {3,5,9,2,8,10,11}, 17
+	// {2,3,5,8,9,10,11}, 17
+
+	for i <= j {
+		result := abs(input[j] + input[i])
+
+		if result == target {
+			return []int{input[i], input[j]}
+		}
+
+		if result < target {
+			i++
+		} else {
+			j--
+		}
+	}
+
+	return []int{}
+}
+
 func main() {
 	a := []int{3, 5, -4, 8, 11, 1, -1, 6}
 	givenSum := 10
 	fmt.Println(TwoNumberSum(a, givenSum))
+	a = []int{2, 3, 5, 8, 9, 10, 11}
+	fmt.Println(TwoNumberSum2PointerApproach(a, 17))
+	a = []int{3, 5, 9, 2, 8, 10, 11}
+	fmt.Println(TwoNumberSum2PointerApproach(a, 10)) // Two pointer approach fails for unsorted array
 }
--- go/Arrays/majority_element.go
+++ go/Arrays/majority_element.go
@@ -21,9 +21,35 @@ func MajorityElement(input []int) []int {
 	return result
 }
 
+// func MajorityElementUsingSorting(input []int) int {
+// 	sort.Ints(input)
+// 	maxElement := input[0]
+// 	maxIndex := 0
+
+// 	for i := 1; i < len(input); i++ {
+// 		maxCount := 1
+
+// 		/*
+// 			While i + 1 < N && input[i] == input[i+1]{
+// 				count = count + 1
+// 			}
+
+// 			if  count > N/2 {
+// 				print maxElement
+// 				brea
+// 			}
+// 		*/
+// 		if input[i] == maxElement {
+// 			maxElement = input[i]
+// 			maxCount += 1
+// 		}
+
+// 	}
+// }
+
 /*
 	- Find the Majority
-	- Print the indexes of the Majority
+	- Print the Majority
 */
 
 func FindMajority(input []int) int { //{8, 3, 8, 1, 8, 2, 8, 9, 8}
@@ -44,6 +70,26 @@ func FindMajority(input []int) int { //{8, 3, 8, 1, 8, 2, 8, 9, 8}
 	}
 
 	return input[majority]
+}
+
+func FindMaxOccuringElem(input []int) int { //{7,8,7,7,8,7,7,9}
+	index := 0
+	count := 1
+
+	for i := 1; i < len(input); i++ {
+		if input[index] == input[i] {
+			count++
+		} else {
+			count--
+		}
+
+		if count == 0 {
+			index = i
+			count = 1
+		}
+	}
+
+	return input[index]
 }
 
 func MajorityElementEfficient(input []int) int {
@@ -70,4 +116,6 @@ func main() {
 	a = []int{8, 3, 8, 1, 8, 2, 8, 9}
 	fmt.Println(MajorityElement(a))
 	fmt.Println(MajorityElementEfficient(a))
+	a = []int{7, 8, 7, 7, 8, 7, 7, 9}
+	fmt.Println(FindMaxOccuringElem(a))
 }
